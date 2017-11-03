ee5c5008599f887d70f74b32b7f195289c36189b
Tejas Kumar <tejas@contiamo.com>
2017-11-03 17:55:59 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# Basically, Continuous Deployment
+
+This project was created to help introduce the concept of _Continuous Deployment_ to anyone interested.
+
+_Basically_ how it works is:
+
+*   You `git push` to a GitHub (or something else) server.
+*   The `git` server uses a WebHook to connect with a CI/CD system.
+*   The CI/CD thing builds your project, and deploys it somewhere.
+
+Simple, right? This webpage actually, was built with a Continuous Deployment script!
+
+[Check out the source code](#) to understand how it works.
+
+## How does _this_ project work?
+
+Glad you asked!
+
+*   It was created with [`create-react-app`](https://github.com/facebookincubator/create-react-app).
+*   It uses [Travis CI](https://travis-ci.org/) as the CI/CD thing, which starts a new build on `git push`.
+*   When Travis starts a new build, it executes [this script](#).
+    *   It is instructed to use the script, with [_this_ script.](#)
+*   I highly recommend reading the deploy script above. It is heavily documented for you in true _Basically_ fashion, in order to help you understand what's actually happening.
+*   After Travis is done, everything's deployed on [`gh-pages`](#), and things are live.
+
+## What's this about keys? 🗝
+
+Basically, Travis is going to be pushing to your GitHub account. Travis needs to be able to say HEY IM TEJAS in order to use Tejas' GitHub: it needs my key; my SSH key.
+
+And so, I've got to:
+
+*   Generate some keys:
+    *   In a terminal, type:  
+        `ssh-keygen -t rsa -b 4096 -C "hello@tej.as" # YOUR EMAIL HERE`
+    *   It'll ask you where to save it. Save the key somewhere familiar.
+    *   It'll ask you for a passphrase. I usually leave this blank.
+    *   It'll generate 2 files for you:
+        *   One ending with .pub **(make a note of this)**.
+        *   And one with the name you gave it.
+*   Encrypt them with Travis:
+    *   Make sure you have the{" "} [Travis CLI](https://github.com/travis-ci/travis.rb) {" "} installed.
+    *   In the terminal, run:  
+        `travis encrypt-file WHATEVER_YOU_NAMED_YOUR_KEY`
+    *   It will then create a `.enc` file based your key's filename.
+    *   It'll also say something back to you like:  
+        `openssl aes-256-cbc -K $encrypted_ <span style="{{" color:="" "#0f0",="" fontweight:="" "bold"="" }}="">0a6446eb3ae3</span> _key -iv $encrypted_0a6446eb3ae3_key -blah blah`
+    *   Copy the portion where we have <span style="{{" color:="" "green"="" }}="">green</span> text above. You'll need it.
+*   Add them (the _encrypted ones!_) to my project:
+    *   At this point, you have add your .enc file to your git repo and commit it.
+    *   You can throw away your key at this point.
+*   Tell Travis how to decrypt them:
+    *   In your [.travis.yml](#), you'll want to add an `ENCRYPTION_LABEL` with that red thing you copied above.
+    *   See [this project's .travis.yml](#) as an example.
+*   Add the keys to my GitHub account:
+    *   The last step is actually adding the public part of your key to your GitHub profile to say "yes, the Travis thing using my key is basically me".
+    *   [Go here](https://github.com/settings/keys) , click the green **New SSH Key**, and paste the contents of your .pub file in the{" "} `key` field, giving it an appropriate title.
+    *   Bam!
+
+
+Whew! Now, Travis can properly push your shiny new `gh-pages` to your GitHub project.
---
+++ deploy.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+# The above thing is needed for bash
+set -e # Exit with nonzero exit code if anything fails
+
+# Let's tell Travis we're in production.
+export NODE_ENV='production'
+
+# So if master is pushed, update gh-pages.
+SOURCE_BRANCH="master"
+TARGET_BRANCH="gh-pages"
+
+# A simple alias for our build command.
+function doCompile {
+  npm run build
+}
+
+# If someone just opened a pull request, or pushed some other branch,
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+
+    # Just build and exit.
+    doCompile
+    exit 0
+fi
+
+# If not, store some variables.
+REPO=`git config remote.origin.url`
+REPO_LINK=${REPO/https:\/\/github.com\//git@github.com:}
+COMMIT_HASH=`git rev-parse --verify HEAD`
+
+# Clone this repo into a folder (on Travis) called "clone"
+git clone $REPO clone
+
+# Go into this folder
+cd clone
+
+# If we fail to check out the target branch as a new branch
+# Or, if the target branch (gh-pages) already exists,
+if ! git checkout -b $TARGET_BRANCH $SOURCE_BRANCH; then
+
+  # Delete it
+  git branch -D $TARGET_BRANCH
+
+  # Create a copy of the source branch (master) named target branch (gh-pages)
+  git checkout -b $TARGET_BRANCH $SOURCE_BRANCH
+
+# End conditional
+fi
+
+# Copy over all the node modules so we don't have to `yarn install` again.
+cp -a ../node_modules node_modules
+
+# Run our compile script, inside the built folder.
+# This is expected to create a `build` folder with production, compiled code.
+# If your build script does something else, please account for in this file.
+doCompile
+
+# Find all files that are not:
+#   - our deployment keys (keys.enc)
+#   - our final compiled code (build)
+#   - the .git folder (LOL)
+#   - the current folder (. – also LOL)
+#   - don't be recursive
+#
+# and then for each found file,
+# delete it.
+find . ! -name keys.enc ! -name build ! -name .git ! -name . -maxdepth 1 -exec rm -rf {} +
+
+# Get the current directory.
+cwd=`pwd`
+
+# Copy the contents of the build folder into this current directory.
+cp -a "${cwd}/build"/. "${cwd}"
+
+# Now since the `build` stuff is in the root, delete build.
+rm -rf build
+
+# Close the index.html to 404.html
+# On gh-pages, this allows you to use react-router and other JS routers for your site.
+cp index.html 404.html # react-router woooo
+
+# You can configure this git instance to have its own name and email!
+git config user.name "Travis Kumar"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+# If nothing changed,
+if git diff --quiet; then
+    echo "Nothing changed."
+
+    # die.
+    exit 0
+fi
+
+# Otherwise, add all files.
+git add -A .
+
+# Commit them with a message including the hash.
+git commit -m "Deploy to GitHub Pages: ${COMMIT_HASH}"
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+# THIS STEP IS KEY! How do I get keys?!
+# https://tejasq.github.io/basically-continuous-deployment/index.html#keys
+
+# Store some variables
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+
+# Use the key:
+# openssl [type of encryption] -K [key] -iv [initialization vector]
+# An initialization vector is basically a random number that guarantees the encrypted text is unique, that is used
+# in encrypting AND decrypting something.
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in keys.enc -out key -d
+
+# Update permissions
+chmod 600 key
+
+# Add key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+# Push the new gh-pages with the -f tag, overwriting it completely.
+# Basically, this guarantees that `gh-pages` is ALWAYS 1 commit ahead of master, with the latest production code.
+git push -f -u $REPO_LINK $TARGET_BRANCH
+
+# Thank you for reading. Happy internetting! 🚀
